c36680398c8bbcea8c06fb90bd6edd5af19e48ff
Gourav Gupta <gourav190898@gmail.com>
2022-01-10 13:50:56 +0530

    CHanging welcome message

--- demo/src/main/java/com/test/demo/Test.java
+++ demo/src/main/java/com/test/demo/Test.java
@@ -4,6 +4,6 @@ public class Test {
 
 	  public static void main(String[] args) {
 		  
-		  System.out.println("jay shree ganeshay namah");
+		  System.out.println("Application has started");
 	  }
 }
